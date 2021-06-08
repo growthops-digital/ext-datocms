a167c903d2964519b3ed6758be8e7b87b08c9224
Tristan Strathearn <56568247+tristan-go@users.noreply.github.com>
2021-06-08 16:15:35 +1000

    feat: add renderBlocks function

---
+++ src/render-blocks.test.js
@@ -0,0 +1,39 @@
+import renderBlocks from './render-blocks.js';
+
+test('renders simple blocks as expected', () => {
+	const blocks = [
+		{
+			_modelApiKey: 'foo',
+			content: 'bar'
+		}
+	];
+
+	const renderFooBlock = jest.fn(data => data.content);
+
+	const result = renderBlocks({
+		foo: renderFooBlock
+	}, blocks);
+
+	expect(renderFooBlock).toHaveBeenCalled();
+	expect(result).toEqual(expect.arrayContaining(['bar']));
+});
+
+test('renders simple blocks as expected — curried', () => {
+	const blocks = [
+		{
+			_modelApiKey: 'foo',
+			content: 'bar'
+		}
+	];
+
+	const renderFooBlock = jest.fn(data => data.content);
+
+	const render = renderBlocks({
+		foo: renderFooBlock
+	});
+
+	const result = render(blocks);
+
+	expect(renderFooBlock).toHaveBeenCalled();
+	expect(result).toEqual(expect.arrayContaining(['bar']));
+});
